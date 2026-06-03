84697e17282fcffb978927605840205891196cd7
Taufik <mzatfk@gmail.com>
2026-06-03 11:29:18 +0700

    nih foldernya woi

---
+++ src/main/java/com/kelbongcin/sistem_mahasiswa/SistemMahasiswaApplication.java
@@ -0,0 +1,13 @@
+package com.kelbongcin.sistem_mahasiswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemMahasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemMahasiswaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kelbongcin/sistem_mahasiswa/SistemMahasiswaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelbongcin.sistem_mahasiswa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemMahasiswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
