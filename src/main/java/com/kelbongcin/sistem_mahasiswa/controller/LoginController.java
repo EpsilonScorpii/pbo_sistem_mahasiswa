5b51bdca1bcdecd8bb7ee321766215d31f1dde96
Kyohei <lutfiarsya34@gmail.com>
2026-06-16 21:47:42 +0700

    Update: Regex Validation

--- src/main/java/com/kelbongcin/sistem_mahasiswa/controller/LoginController.java
+++ src/main/java/com/kelbongcin/sistem_mahasiswa/controller/LoginController.java
@@ -2,7 +2,9 @@ package com.kelbongcin.sistem_mahasiswa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.util.regex.Pattern;
 
 import com.kelbongcin.sistem_mahasiswa.model.User;
 import com.kelbongcin.sistem_mahasiswa.service.UserService;
@@ -12,6 +14,8 @@ public class LoginController {
 
     @Autowired
     private UserService service;
+    String GmailPattern =  "^[A-Za-z0-9._%+-]+@gmail\\.com$";
+    
 
     @GetMapping("/login")
     public String loginPage() {
@@ -21,10 +25,23 @@ public class LoginController {
     @PostMapping("/login")
     public String login(
             @RequestParam String email,
-            @RequestParam String password) {
+            @RequestParam String password, Model model) {
 
         User user = service.login(email, password);
-        System.out.println("Login attempt: " + email + " / " + password);
+        boolean isEmailValid = Pattern.matches(GmailPattern, email);
+
+        if (!isEmailValid) {
+        model.addAttribute(
+            "emailError",
+            "Format email tidak valid");
+        return "login";
+    }
+
+
+        if (!isEmailValid) {
+            return "redirect:/login?error";
+        }
+
         if (user == null) {
             return "redirect:/login?error";
         }
