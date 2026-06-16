bd3bc3cbf424a483226d0319a60f002a44ea6d11
Kyohei <lutfiarsya34@gmail.com>
2026-06-16 19:22:17 +0700

    Update: Add User java to validate with database

---
+++ src/main/java/com/kelbongcin/sistem_mahasiswa/model/User.java
@@ -0,0 +1,45 @@
+package com.kelbongcin.sistem_mahasiswa.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_users;
+
+    private String email;
+    private String password;
+    private String roles;
+    public User() {}
+
+    public Long getId() {
+        return id_users;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
